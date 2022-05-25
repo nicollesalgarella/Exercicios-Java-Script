5518efee87ee412c20bf8a9ccf521d0e60d90ebc
Nickzinha <princesinhaonedirection4@gmail.com>
2022-05-25 19:17:53 -0300

    Exercícios JS

--- media.js
+++ media.js
@@ -1,19 +1,25 @@
-function Média(){
-    let n= 1
-    let P= 0
-    let I= 0
-
-    while (n <= 10)
-    a = int(input())
-    n = n + 1
-    if ( 2 == 0){
-    P = P + 1
+function media() {
+    
+    let numero = parseFloat(0);
+    let i = (0);
+    let n = parseInt(0);
+  
+    while (i<=100){
+      let n = parseInt(prompt("Digite um Numero"));
+      
+  
+      if (n % 2 == 0) {
+        numero = numero + n;
+          i = i +1
+      }
+       if (n == 0) {
+        alert("Finalizando");
+        break;
+      }
+  
     }
-
-    else if (I = I + 1){
-
-    print("A qtd de números pares é: ", P)
-    print("A qtd de números ímpares é: ", P)
-    } 
+  
+    alert("A média é = " + numero / (i - 1));
+    
+    console.log(i)
 }
-
